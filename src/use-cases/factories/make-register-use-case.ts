9b199d23247ef7a9ae42785c0ba58ec81b9eee31
JOAO PEDRO LOPES VICENTIN <jplvicen@gmail.com>
2024-12-30 15:03:28 -0300

    feat: implementando casos de uso

---
+++ src/use-cases/factories/make-register-use-case.ts
@@ -0,0 +1,8 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-user-repository'
+import { RegisterUseCase } from '../resgister'
+
+export function makeRegisterUseCase() {
+  const usersRepository = new PrismaUsersRepository()
+  const registerUseCase = new RegisterUseCase(usersRepository)
+  return registerUseCase
+}
